894c4f1c0180ede205b791f9c594e22eed61e3b7
Shyam <butanishyam111@gmail.com>
2022-12-29 02:55:30 +0530

    Exchanges page completed
    Loader and Error components are temporaray yet

---
+++ src/components/ErrorComponent.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ErrorComponent = ({ message }) => {
+  return <div>{message}</div>;
+};
+
+export default ErrorComponent;
--- src/components/Exchanges.jsx
+++ src/components/Exchanges.jsx
@@ -1,7 +1,92 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import {
+  Container,
+  Heading,
+  HStack,
+  Image,
+  Text,
+  VStack,
+} from "@chakra-ui/react";
+import { server } from "../index";
+import Loader from "./Loader";
+import ErrorComponent from "./ErrorComponent";
 
 const Exchanges = () => {
-  return <div>Exchanges</div>;
+  const [exchanges, setExchanges] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(false);
+
+  useEffect(() => {
+    const fetchExchanges = async () => {
+      try {
+        const { data } = await axios.get(`${server}/exchanges`);
+        setExchanges(data);
+        setLoading(false);
+      } catch (error) {
+        setError(true);
+        setLoading(false);
+      }
+    };
+
+    fetchExchanges();
+  }, []);
+
+  if (error) return <ErrorComponent message="Error while fetching exchanges"/>;
+
+  return (
+    <Container maxW={"container.xl"}>
+      {loading ? (
+        <Loader />
+      ) : (
+        <>
+          <HStack wrap={"wrap"}>
+            {exchanges.map((i) => (
+              <ExchangeCard
+                key={i.id}
+                name={i.name}
+                img={i.image}
+                url={i.url}
+                rank={i.trust_score_rank}
+              />
+            ))}
+          </HStack>
+        </>
+      )}
+    </Container>
+  );
 };
+
+const ExchangeCard = ({ name, img, url, rank }) => (
+  <a href={url} target={"blank"}>
+    <VStack
+      w={"52"}
+      m={"4"}
+      p={"8"}
+      shadow={"lg"}
+      borderRadius={"lg"}
+      transition={"all 0.3s"}
+      css={{
+        "&:hover": {
+          transform: "scale(1.1)",
+        },
+      }}
+    >
+      <Image
+        src={img}
+        w={"10"}
+        h={"10"}
+        alt={"Exchange"}
+        objectFit={"contain"}
+      />
+
+      <Heading size={"md"} noOfLines={1}>
+        {rank}
+      </Heading>
+
+      <Text noOfLines={1}>{name}</Text>
+    </VStack>
+  </a>
+);
 
 export default Exchanges;
