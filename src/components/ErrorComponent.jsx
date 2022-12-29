c336b9911e6b9db55a3d775f2919fc30bc53b104
Shyam <butanishyam111@gmail.com>
2022-12-29 08:32:24 +0530

    Error component completed

--- src/components/ErrorComponent.jsx
+++ src/components/ErrorComponent.jsx
@@ -1,7 +1,20 @@
+import { Alert, AlertIcon } from "@chakra-ui/react";
 import React from "react";
 
 const ErrorComponent = ({ message }) => {
-  return <div>{message}</div>;
+  return (
+    <Alert
+      status={"error"}
+      width={"container.lg"}
+      position={"fixed"}
+      bottom={"5"}
+      left={"50%"}
+      transform={"translateX(-50%)"}
+    >
+      <AlertIcon />
+      {message}
+    </Alert>
+  );
 };
 
 export default ErrorComponent;
