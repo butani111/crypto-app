2af487dc18959e62a0739562e4f68c302d68ca2d
Shyam <butanishyam111@gmail.com>
2022-12-29 08:15:43 +0530

    Loader component completed

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -1,7 +1,14 @@
 import React from "react";
+import { Box, Spinner, VStack } from "@chakra-ui/react";
 
 const Loader = () => {
-  return <div>Loader</div>;
+  return (
+    <VStack h={"90vh"} justifyContent={"center"}>
+      <Box transform={"scale(3)"}>
+        <Spinner size={"xl"} />
+      </Box>
+    </VStack>
+  );
 };
 
 export default Loader;
