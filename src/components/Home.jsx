4bbe647634109bfa8754de7c4f2bf5b374a94fdc
Shyam <butanishyam111@gmail.com>
2022-12-29 18:40:47 +0530

    Animation added on Home page image

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,11 +1,18 @@
 import React from "react";
 import { Box, Image, Text } from "@chakra-ui/react";
 import bitcoinImg from "../assets/bitcoin.png";
+import { motion } from "framer-motion";
 
 const Home = () => {
   return (
     <Box w={"full"} h={"85vh"} bgColor={"blackAlpha.900"}>
-      <Image src={bitcoinImg} w={"full"} h={"full"} objectFit={"contain"} />
+      <motion.div
+        style={{ height: "80vh" }}
+        animate={{ translateY: "20px" }}
+        transition={{ duration: 2, repeat: Infinity, repeatType: "reverse" }}
+      >
+        <Image src={bitcoinImg} w={"full"} h={"full"} objectFit={"contain"} />
+      </motion.div>
 
       <Text
         color={"whiteAlpha.700"}
