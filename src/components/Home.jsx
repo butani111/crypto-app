76ab4ab29733fb6d646809ee442c74ede8f286ab
Shyam <butanishyam111@gmail.com>
2022-12-29 18:10:46 +0530

    Home page completed

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,7 +1,23 @@
 import React from "react";
+import { Box, Image, Text } from "@chakra-ui/react";
+import bitcoinImg from "../assets/bitcoin.png";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <Box w={"full"} h={"85vh"} bgColor={"blackAlpha.900"}>
+      <Image src={bitcoinImg} w={"full"} h={"full"} objectFit={"contain"} />
+
+      <Text
+        color={"whiteAlpha.700"}
+        fontSize={"6xl"}
+        fontWeight={"thin"}
+        textAlign={"center"}
+        mt={"-20"}
+      >
+        SBcrypto
+      </Text>
+    </Box>
+  );
 };
 
 export default Home;
